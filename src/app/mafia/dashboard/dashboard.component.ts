165c11c7387501b34c2c608142f78d2f133196c4
Devvrat Sharma <60777422+devrats@users.noreply.github.com>
2023-02-27 20:20:34 +0530

    swal implemented

--- src/app/mafia/dashboard/dashboard.component.ts
+++ src/app/mafia/dashboard/dashboard.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-import { CircleProgressComponent, CircleProgressOptions } from 'ng-circle-progress';
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  constructor(){}
+
+  ngOnInit(): void {
+  }
+
+  createGame(){
+    console.log("hello");
+    Swal.fire({
+      icon: 'success',
+      title: 'Game created successfully',
+      html: 'Game Code : 123456 </br> Share Code With your Friends',
+      confirmButtonText: 'Share Code'
+    })
+  }
 
 }
